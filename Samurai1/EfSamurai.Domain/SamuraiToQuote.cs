1266744b5b6d163fa09bf3c7362882f37dc3f6e0
saraglavin <hej@saraglavin.se>
2019-01-17 14:00:13 +0100

    Mer klasser

---
+++ Samurai1/EfSamurai.Domain/SamuraiToQuote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfSamurai.Domain
+{
+    class SamuraiToQuote
+    {
+        public int SamuraiId { get; set; }
+        public Samurai Samurai { get; set; }
+
+        public int QuoteId { get; set; }
+        public Quote Quote { get; set; }
+
+    }
+}
